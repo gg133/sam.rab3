1cc82ac3f040d852d22359c2b1e4d2d72e20edb3
gg133 <133chess@gmail.com>
2021-10-04 11:15:38 +0300

    Добавьте файлы проекта.

---
+++ sm.rab3/sm.rab3.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float number1;
+	float number2;
+	float number3;
+	cout << "введите сначало 1-е число, потом 2-е\n";
+	cin >> number1 >> number2;
+	cout << "Введите знак операции\n";
+	char operation;
+	cin >> operation;
+	if (operation == '+') {
+		number3 = number1+ number2;
+		cout << "Результат: " << number1 << " + " << number2 << " = " << number3;
+	}
+	else if (operation == '-') {
+		number3 = number1 - number2;
+		cout << "Результат: " << number1 << " - " << number2 << " = " << number3;
+	}
+	else if (operation == '*') {
+		number3 = number1 * number2;
+		cout << "Результат: " << number1 << " * " << number2 << " = " << number3;
+	}
+	else if (operation == '/') {
+		if (number2 == 0)
+		{
+			cout << "На ноль делить нельзя)\n";
+		}
+		else {
+			number3 = number1 / number2;
+			cout << "Результат: " << number1 << " / " << number2 << " = " << number3;
+		}
+	}
+	else if (operation == '%') {
+		number3 = (int)number1 % (int)number2;
+		cout <<"Результат: "<<number1<<" % " << number2 << " = " << number3;
+	}
+	else {
+		cout << "вы ввели неверный знак операции\n";
+	}
+	int _;
+	cin >> _;
+	return 0;
+}
+
